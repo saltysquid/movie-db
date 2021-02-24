9a301bb144c6e3f4b2de1f63a646c0f37debcb5a
Brian Legg <brianlegg99@gmail.com>
2021-02-23 23:48:47 -0500

    final

--- src/context.js
+++ src/context.js
@@ -1,14 +1,28 @@
-import React, { useState, useContext, useEffect } from 'react'
-// make sure to use https
-export const API_ENDPOINT = `https://www.omdbapi.com/?apikey=${process.env.REACT_APP_MOVIE_API_KEY}`
-const AppContext = React.createContext()
+import React, { useState, useContext, useEffect } from "react";
+import useFetch from "./useFetch";
+
+const AppContext = React.createContext();
 
 const AppProvider = ({ children }) => {
-  return <AppContext.Provider value='hello'>{children}</AppContext.Provider>
-}
-// make sure use
-export const useGlobalContext = () => {
-  return useContext(AppContext)
-}
+  const [query, setQuery] = useState("car");
+  const { isLoading, error, data: movies } = useFetch(`&s=${query}`);
 
-export { AppContext, AppProvider }
+  return (
+    <AppContext.Provider
+      value={{
+        isLoading,
+        error,
+        movies,
+        setQuery,
+      }}
+    >
+      {children}
+    </AppContext.Provider>
+  );
+};
+
+export const useGlobalContext = () => {
+  return useContext(AppContext);
+};
+
+export { AppContext, AppProvider };
